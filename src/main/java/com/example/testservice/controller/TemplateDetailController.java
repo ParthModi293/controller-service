f6cfebb25d5619be3b18b93089cb3a6eca8edbda
zeelboghara <zeel@bizott.com>
2025-02-15 18:02:39 +0530

    log and comment changes

--- src/main/java/com/example/testservice/controller/TemplateDetailController.java
+++ src/main/java/com/example/testservice/controller/TemplateDetailController.java
@@ -1,14 +1,18 @@
 package com.example.testservice.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.common.common.ResponseBean;
 import org.communication.dto.TemplateDetailsDto;
 import org.communication.service.TemplateDetailsService;
+import org.jsoup.nodes.Document;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/template-detail")
+@Log4j2
 public class TemplateDetailController {
 
     private final TemplateDetailsService templateDetailsService;
@@ -19,18 +23,21 @@ public class TemplateDetailController {
 
     @PostMapping
     public ResponseEntity<?> createTemplateDetail(@RequestBody @Valid TemplateDetailsDto templateDetailsDto) {
+        log.info("create template detail : {}",templateDetailsDto);
         ResponseBean<?> responseBean = templateDetailsService.createTemplateDetail(templateDetailsDto);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
 
     @GetMapping("/{templateMastId}")
     public ResponseEntity<?> getTemplateDetail(@PathVariable("templateMastId") int templateMastId) {
+        log.info("get template detail on template mast id: {}",templateMastId);
         ResponseBean<?> responseBean = templateDetailsService.getTemplateDetail(templateMastId);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
 
     @GetMapping("getAll/{templateMastId}")
     public ResponseEntity<?> getAllTemplateDetail(@PathVariable("templateMastId") int templateMastId) {
+        log.info("get all template detail on template mast id: {}",templateMastId);
         ResponseBean<?> responseBean = templateDetailsService.getAllTemplateDetail(templateMastId);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
--- src/main/java/com/example/testservice/controller/TemplateMastController.java
+++ src/main/java/com/example/testservice/controller/TemplateMastController.java
@@ -1,6 +1,7 @@
 package com.example.testservice.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.common.common.ResponseBean;
 import org.communication.dto.TemplateMastDto;
 import org.communication.dto.TemplateMastFilterRequest;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/template-mast")
+@Log4j2
 public class TemplateMastController {
 
     private final TemplateMastService templateMastService;
@@ -20,18 +22,21 @@ public class TemplateMastController {
 
     @PostMapping
     public ResponseEntity<?> createTemplate(@RequestBody @Valid TemplateMastDto templateMastDto) {
+        log.info("create template mast : {}",templateMastDto);
         ResponseBean<?> responseBean = templateMastService.createTemplate(templateMastDto);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
 
     @GetMapping
     public ResponseEntity<?> getAllTemplates(@RequestBody @Valid TemplateMastFilterRequest templateMastFilterRequest) {
+        log.info("get all template mast on event : {}",templateMastFilterRequest);
         ResponseBean<?> responseBean = templateMastService.getAllTemplates(templateMastFilterRequest);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getTemplateById(@PathVariable int id) {
+        log.info("get template mast on id : {}",id);
         ResponseBean<?> responseBean = templateMastService.getTemplateById(id);
         return new ResponseEntity<>(responseBean, responseBean.getRStatus());
     }
